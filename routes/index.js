7033b83838829a169ae92baf04e51c0210f1f1f6
giakDanz <20141742@aloe.ulima.edu.pe>
2021-09-02 01:50:00 -0500

    v 1.3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('productos', { title: 'Productos - MegaVision' });
 });
 
 module.exports = router;
--- routes/quienesSomos.js
+++ routes/quienesSomos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('quienesSomos', { title: 'Quienes Somos' });
+    res.render('quienesSomos', { title: 'Quienes Somos - MegaVision' });
 });
 
 module.exports = router;
